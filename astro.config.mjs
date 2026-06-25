7674def2fa13937042e662aa3dcba82508ffbca0
Kanak Monika Kanphade <kanak.kanphade@gmail.com>
2026-06-25 19:58:58 -0400

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Kanakmk.github.io',
+  site: 'https://kanakmk.github.io',
   base: '/CafeMon',
   vite: {
     plugins: [tailwindcss()]
